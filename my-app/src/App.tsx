b10ca9e49f7f3776822e06de3a7c821a7888c266
isabellearaujod <isabelledantasaraujo@gmail.com>
2026-08-12 21:03:40 -0300

    feat : Meu primeiro componente REACT integrado.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import Isabelle from "./componets/isabelle/isabelle";
+
+export default function App() {
+    //Declarar constantes e variaveis
+
+  return(
+      <div>
+      {/** aqui voce cria todo conteudo HTML de apresenaçao */}
+        <h1>Meu Componente</h1>
+        <Isabelle/>
+      </div>
+  );
+
+}
