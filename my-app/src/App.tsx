5105bbb1bac613d3467a13d868468233a847530a
velascoguzmanvictorhugo-ui <velascoguzmanvictorhugo@gmail.com>
2026-08-12 21:05:46 -0300

    feat: meu primeiro comoponete REACT

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import victor from ".components/victor/victor";
+
+export default function App() {
+  //declarar constatante e variaveis
+
+  return (
+    <div>
+      {/** Aqui voce cria todo conteudo HTML de apresentasao */}
+      <h1>
+        Meu componente
+      </h1>
+      <victor />
+
+
+    </div>
+
+
+  );
+}
---
+++ my-app/src/components/victor/victor.tsx
@@ -0,0 +1,6 @@
+
+rt default function victor() {
+    return (
+        <h2>victor hugo</h2>
+    );
+}
