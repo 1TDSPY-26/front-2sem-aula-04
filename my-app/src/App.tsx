be7f300be2fc4994bb98cc6d735e5d5e0e297c00
tecnando-rpa <pronandofinancas@gmail.com>
2026-08-12 21:27:00 -0300

    feat: Meu primeiro componente React Integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import Fer from "./components/Fer/Fer";
+
+
+export default function App(){
+  //Declarar constantes de variáveis
+
+
+  return (
+    <div>
+      {/** Aqui você cria todo o conteúdo do HTML de
+      apresentação */}
+        <h1>Fer</h1>
+        <Fer />
+   </div>  
+  
+  );
+    
+
+}
---
+++ my-app/src/components/Fer/Fer.tsx
@@ -0,0 +1,7 @@
+
+export default function Fer(){
+  return (
+    <h2>Fer</h2>
+      
+  );
+}
