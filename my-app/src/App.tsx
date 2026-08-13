90920ea0260828ae53fc651d42bcdcd0649ccdf9
rodrigobertolla <guigobs2008@gmail.com>
2026-08-12 21:04:06 -0300

    feat: eu primeiro componente REACT integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import Rodrigo from "./components/Rodrigo/Rodrigo";
+
+export default function App(){
+  //declarar constantes e variaveis
+  
+  return(
+
+    <div>
+      (/** aqui você cria todo o conteúo HTML de sua apresentação*)
+      <h1>Meu Componente</h1>
+      <Rodrigo/>
+    </div>
+  )
+
+   
+}
---
+++ my-app/src/components/Rodrigo/Rodrigo.tsx
@@ -0,0 +1,5 @@
+export default function Rodrigo(){
+    return(
+        <h2>Rodrigo</h2>
+    );
+}
