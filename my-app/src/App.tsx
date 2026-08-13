d068c84241ea1a2488c8122ceaab761fcaa1a052
jonatanfeitosa <jonatanvieiraf2@gmail.com>
2026-08-12 21:03:44 -0300

    feat : meu primeiro componente REACT integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import Jonatan from "./components/Jonatan/Jonatan";
+
+export default function App() {
+  //declarar constantes e variáveis 
+  
+
+
+  return(
+
+    
+  <div>
+    <h1>Meu Componente</h1>
+    <Jonatan/>
+
+  </div>
+
+
+  );
+
+}
---
+++ my-app/src/components/Jonatan/Jonatan.tsx
@@ -0,0 +1,7 @@
+export default function Jonatan(){
+
+    return(
+        <h2>Jonatan</h2>
+
+    );
+}
