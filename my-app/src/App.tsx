a4dfcb0dd7ad492e82f7b6a1613681edb0a44255
vitorf8 <vitor.rf08@gmail.com>
2026-08-12 21:04:13 -0300

    feat: Meu primeiro componente REACT integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import Vitor from "./components/Vitor/Vitor";
+
+export default function App() {
+
+  //Declarar constantes e variaveis
+  return(
+    <div>
+      {
+      //Aqui vc cria todo o conteudo HTML de apresentacao
+      }
+
+      <h1>Meu componente</h1>
+      
+    <Vitor/>
+
+    </div>
+
+  );
+
+}
---
+++ my-app/src/components/Vitor/Vitor.tsx
@@ -0,0 +1,10 @@
+export default function Vitor() {
+
+    return (
+        <div>
+            <h2>Seu nome</h2>
+        </div>
+
+    );
+
+}
