56b8d84b891fe5526216fa63efffd899ee5a7a7c
Lagsystems <lauraalbuquerquegama2008@gmail.com>
2026-08-12 21:05:16 -0300

    Feat: meu primeiro componente react integrado

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import Laura from "./Laura/Laura";
+
+export default function App() {
+  //Declarar constante e variáveis
+
+  return(
+    
+    <div>
+      
+       <h1>Meu componente</h1>
+       <Laura></Laura>
+      
+    </div>
+
+  
+  );
+}
---
+++ my-app/src/Laura/Laura.tsx
@@ -0,0 +1,5 @@
+export default function Laura() {
+    return(
+        <h2>Laura</h2>
+    )
+}
