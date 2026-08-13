362ec0c988e7b4511c885ccc1e292ed65036e5e7
isDevZ <dfc1100@hotmail.com>
2026-08-12 21:03:25 -0300

    feat : Meu primeiro componente REACT integrado.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Davi from "./Davi/Davi";
+
+function App() {
+  //declarar constantes e variaveis
+
+  return (
+    <div>
+      <h1>Meu componente</h1>
+      <Davi />
+    </div>
+
+  );
+}
---
+++ my-app/src/Davi/Davi.tsx
@@ -0,0 +1,5 @@
+export default function Davi(){
+    return(
+        <h2>Menor Quentao</h2>
+    )
+}
