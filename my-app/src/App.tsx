8d94bab8e5fda18407beaf6bc6840c0f7b8b9b5d
felipe-santosdev <contato.felipesantos27@gmail.com>
2026-08-12 21:06:12 -0300

    feat: Meu primeiro componente REACT.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Felipe from "./components/Felipe/Felipe";
+
+export default function App() {
+
+  //Declarar constantes e variáveis
+  return (
+    <div>
+      <h1>Meu componente</h1>
+      <Felipe/>
+    </div>
+  );
+}
---
+++ my-app/src/components/Felipe/Felipe.tsx
@@ -0,0 +1,5 @@
+export default function Felipe(){
+    return(
+        <h2>Felipe</h2>
+    );
+}
